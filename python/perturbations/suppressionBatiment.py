2b544d0bf2cd9e47bf0e1a5013d1a067a1deefc3
fe73 <93479357+fe73@users.noreply.github.com>
2022-01-12 00:30:25 +0100

    Update suppressionBatiment.py

--- python/perturbations/suppressionBatiment.py
+++ python/perturbations/suppressionBatiment.py
@@ -25,7 +25,6 @@ def suppression(tauxSuppr,data):
             index = rd.randint(0,nbElementASupr-1)
         listeASuppr.append(index)
     
-    #print(listeASuppr)
     #on supprime les bâtiments correspondants aux indices de la liste listeASuppr
     for index in listeASuppr:
         del data['features'][index]
