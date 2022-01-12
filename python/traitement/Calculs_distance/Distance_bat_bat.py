5c147a9b36047f3d17af70d41b0fbb94c6e20cc8
TFillon <95340968+TFillon@users.noreply.github.com>
2022-01-12 14:19:08 +0100

    Update Distance_bat_bat.py

--- python/traitement/Calculs_distance/Distance_bat_bat.py
+++ python/traitement/Calculs_distance/Distance_bat_bat.py
@@ -1,3 +1,12 @@
+"""
+Auteurs : Tristan FILLON, Vincent HEAU, Félix BAL
+Pour chaque bâtiment de la couche Arcgis stockée dans entree (ligne 22), ce programmme remplit l'attribut NEAR_DIST, contenant la distance au bâtiment le plus proche.
+Etapes nécessaires à l'utilisation de ce script :
+    -Ouvrir un fichier Arcgis et y insérer les fichiers .shp contenant les bâtiments des différentes zones traitées (dans ProjetRecherche/donneesZonesOSM et ProjetRecherche/donneesZonesBDTOPO)
+    -Remplir le contenu des variables scratchWorkspace et workspace (ligne 28). Ils correspondent à l'emplacement de la geodatabase (et se terminent donc par .gdb) du fichier Arcgis créé.
+    -Insérer ce script dans Arcgis et le lancer.
+"""
+
 import arcpy
 
 def Calcul_dist_bat_bat():
