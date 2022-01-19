2b461fe3e775a8e67ce97d43714ed57cf39878af
VincentHeau <93425160+VincentHeau@users.noreply.github.com>
2022-01-19 09:12:32 +0100

    mise à jour code

--- python/perturbations/suppressionBatiment.py
+++ python/perturbations/suppressionBatiment.py
@@ -1,6 +1,10 @@
 
 
 import random as rd
+import numpy as np
+
+
+
 
 def suppression(tauxSuppr,data):
     """
@@ -16,17 +20,17 @@ def suppression(tauxSuppr,data):
     #on calcule le nombre de bâtiments à supprimer et on utilise une liste d'indice au hasard à supprimer
     nbElementASupr = round(tauxSuppr*nbBat)    
     
-    listeASuppr = []
     
-    #on tire au sort les indices à supprimer
     for i in range(nbElementASupr):
-        index = rd.randint(0,nbElementASupr-1)
-        while index in listeASuppr:
-            index = rd.randint(0,nbElementASupr-1)
-        listeASuppr.append(index)
-    
-    #on supprime les bâtiments correspondants aux indices de la liste listeASuppr
-    for index in listeASuppr:
+        index=rd.randint(0,len(data['features'])-1)
         del data['features'][index]
-       
+    
+    
+    
+        
+
+    
+    #on remplace notre fichier de départ par notre fichier modifié
+    #ecrit_json(data,fichier)
+      
     return data
